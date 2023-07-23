1a44ae74b80383046d3578bd5f20f8a56085d389
shazaaly <shaza.aly@gmail.com>
2023-07-23 19:56:01 +0300

    check null

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,9 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, minIndex;
 	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		minIndex = i;
