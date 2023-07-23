5832572bcf18ff146e882eea7fa9c6a34419fae3
shazaaly <shaza.aly@gmail.com>
2023-07-23 18:51:49 +0300

    fix bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,19 +11,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-size_t c, j;
-int temp;
-for (c = 0; c < size; c++)
-{
-for (j = 0; j < size - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-print_array(array, size);
-}
-}
-}
+	size_t c, j;
+	int temp;
+
+	for (c = 0; c < size; c++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
