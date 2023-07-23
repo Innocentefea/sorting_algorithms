14f5cec1f75cf6a6544c0d73fd238a6d202a2a90
shazaaly <shaza.aly@gmail.com>
2023-07-23 18:55:57 +0300

    bubble fix2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
 * bubble_sort - a function that sorts an array of integers in
 * ascending order using the Bubble sort algorithm
@@ -13,9 +14,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t c, j;
 	int temp;
+	int swapped;
 
 	for (c = 0; c < size; c++)
 	{
+		swapped = 0; /* Initialize the swapped flag to 0 */
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,8 +26,14 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array(array, size);
+				swapped = 1; /* Set the swapped flag to 1 */
 			}
 		}
+
+		/*If no two elements were swapped in the inner loop, the array is already sorted */
+		if (swapped == 0)
+			break;
+
+		print_array(array, size);
 	}
 }
