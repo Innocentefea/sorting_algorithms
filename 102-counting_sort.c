25dc37c74ad2389086be10a3cb12e6fec81e1435
shazaaly <shaza.aly@gmail.com>
2023-07-24 10:43:48 +0300

    counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,24 +1,5 @@
 #include "sort.h"
-/**
-* getMax - Find the maximum value in an array.
-* @array: Pointer to the input array.
-* @size: The size of the array.
-* Return: The maximum value in the array.
-*/
-int getMax(int *array, size_t size)
-{
-	size_t i;
-	int max = array[0];
-
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-		{
-			max = array[i];
-		}
-	}
-	return (max);
-}
+#include "getMax.c"
 /**
 * counting_sort - Sorts an array of integers.
 * @array: Pointer to the input array.
